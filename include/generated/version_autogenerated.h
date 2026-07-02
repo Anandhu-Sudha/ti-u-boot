1e38ea2adb4c72cc77b38b17abaf3ec38827e8f6
anandhus-calixto <anandhu.s@calixto.co.in>
2026-07-02 15:29:03 +0530

    major changes

--- include/generated/version_autogenerated.h
+++
@@ -1,7 +0,0 @@
-#define PLAIN_VERSION "2025.01-gcbec03719769-dirty"
-#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
-#define U_BOOT_VERSION_NUM 2025
-#define U_BOOT_VERSION_NUM_PATCH 1
-#define HOST_ARCH 0xaa64
-#define CC_VERSION_STRING "aarch64-none-linux-gnu-gcc (Arm GNU Toolchain 13.3.Rel1 (Build arm-13.24)) 13.3.1 20240614"
-#define LD_VERSION_STRING "GNU ld (Arm GNU Toolchain 13.3.Rel1 (Build arm-13.24)) 2.42.0.20240614"
